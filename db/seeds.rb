6d2b9fe351af7e25774b67cb1eadee7df0e4e15c
Usman Asif <usmanasif@users.noreply.github.com>
2019-07-16 23:23:44 +0500

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 resume = Resume.create(name: "Usman Asif", address: "mail.usmanasif@gmail.com")
 
-Position.create(position: "Full Stack Developer @ Haulhub", description: "asd", year: 2018, resume: resume)
+# Position.create(position: "Full Stack Developer @ Haulhub", description: "Developed software solutions by studying information needs; conferring with users; studying systems flow, data usage and work processes; investigating problem areas; following the software development lifecycle", year: 2018, resume: resume)
 
-Position.create(position: "Lead Developer @ Saplinghr.com", description: "asdasd", year: 2017, resume: resume)
+# Position.create(position: "Lead Developer @ Saplinghr.com", description: "Improved operations by conducting systems analysis; recommending changes in policies and procedures", year: 2017, resume: resume)
 
-Position.create(position: "Rails Developer @ PatientIQ", description: "asdasd", year: 2016, resume: resume)
+# Position.create(position: "Rails Developer @ PatientIQ", description: "Contributed as Ruby on Rails developer. Prepared and installed solutions by determining and designing system specifications, standards and programming", year: 2016, resume: resume)
 
-Position.create(position: "Founder @ EduLeaf", description: "gekki", year: 2015, resume: resume)
+# Position.create(position: "Founder @ EduLeaf", description: "Developed an automated online tool that allows the grading of ordinary paper-based exams containing any type of questions. We can handle math questions, chemical diagrams, essay questions, as well as multiple-choice using EduLeaf.", year: 2015, resume: resume)
